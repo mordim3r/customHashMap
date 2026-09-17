f22f78e8af89e33327a1c48ff1513f448844d650
mordim3r <valent1n.bazylev@gmail.com>
2026-09-17 23:12:15 +0300

    Initial commit

---
+++ src/main/java/CustomHashMap.java
@@ -0,0 +1,58 @@
+public class CustomHashMap <K,V> {
+    int capacity = 16;
+    Node <K,V>[] table;
+
+    public CustomHashMap() {
+        table = new Node[capacity];
+    }
+
+    public static class Node<K,V>{
+        K key;
+        V value;
+        Node <K,V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+    private int index(K key){
+        int hash;
+        if(key==null){
+            hash = 0;
+        } else {
+            hash=Math.abs(key.hashCode());
+        }
+        return hash%capacity;
+    }
+    public V put(K key, V value){
+        int i = index(key);
+        if (table[i]==null){
+            Node<K,V> node = new Node<>(key,value, null);
+            table[i]=node;
+            return null;
+        } else {
+            Node<K,V>current =table[i];
+            while (current!=null){
+                boolean sameKey;
+                if (key==null){
+                    sameKey =  (current.key==null);
+                }else {
+                   sameKey = key.equals(current.key);
+                }
+                if (sameKey){
+                    V oldValue = current.value;
+                    current.value = value;
+                    return oldValue;
+                } else {
+                    current = current.next;
+                }
+            }
+            Node<K,V>newNode = new Node<>(key,value,table[i]);
+            table[i]=newNode;
+            return null;
+        }
+
+    }
+}
